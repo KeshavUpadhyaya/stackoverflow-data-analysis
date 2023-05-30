cdb674a633fbb8acfcb656c74874a0b72f733d68
keshavupadhyaya <31659743+KeshavUpadhyaya@users.noreply.github.com>
2023-05-30 10:47:35 +0200

    completed hyperloglog

--- count_tags.py
+++ count_tags.py
@@ -4,14 +4,37 @@ import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
 from datasketch import HyperLogLog
 
+# Custom combine function for HyperLogLog
+ACCURACY = 14
+
+
+class HyperLogLogCombineFn(beam.CombineFn):
+    def create_accumulator(self):
+        return HyperLogLog(p=ACCURACY)  # p is used to control accuracy, higher is more accuracy
+
+    def add_input(self, accumulator, element):
+        tags = element['tags'].split('|') if element[
+            'tags'] else []  # Split the tags if there are multiple separated by '|'
+        for tag in tags:
+            accumulator.update(tag.encode('utf-8'))  # Encode tag as bytes
+        return accumulator
+
+    def merge_accumulators(self, accumulators):
+        merged_hll = HyperLogLog(p=ACCURACY)
+        for hll in accumulators:
+            merged_hll.merge(hll)
+        return merged_hll
+
+    def extract_output(self, accumulator):
+        return accumulator.count()
+
 
 def run(argv=None):
-    table = 'bigquery-public-data.stackoverflow.posts_questions'
     # Define the query to retrieve the data
     query = f"""
         SELECT tags
-        FROM `{table}`
-        WHERE EXTRACT(YEAR FROM creation_date) BETWEEN 2019 AND 2021
+        FROM `bigquery-public-data.stackoverflow.posts_questions`
+        WHERE EXTRACT(YEAR FROM creation_date) BETWEEN 2008 AND 2009
         LIMIT 1000
     """
 
@@ -26,6 +49,15 @@ def run(argv=None):
             pipeline
             | 'Read from BigQuery' >> beam.io.ReadFromBigQuery(query=query, use_standard_sql=True)
     )
+
+    # Apply the HyperLogLog transform
+    total_distinct_count = (
+            data
+            | beam.CombineGlobally(HyperLogLogCombineFn())
+    )
+
+    # Output the total count of unique tags
+    total_distinct_count | beam.Map(print)
 
     # Execute the pipeline
     result = pipeline.run()
--- datasketch_test.py
+++ datasketch_test.py
@@ -1,45 +1,57 @@
-import apache_beam as beam
-from datasketch import HyperLogLog
+# import apache_beam as beam
+# from datasketch import HyperLogLog
+#
+# # Sample input data
+# data = [
+#     {'tags': 'apple'},
+#     {'tags': 'apple|banana|orange|apple'}
+# ]
+#
+#
+# # Custom combine function for HyperLogLog
+# class HyperLogLogCombineFn(beam.CombineFn):
+#     def create_accumulator(self):
+#         return HyperLogLog()
+#
+#     def add_input(self, accumulator, element):
+#         tags = element.split('|')  # Split the tags if there are multiple separated by '|'
+#         for tag in tags:
+#             accumulator.update(tag.encode('utf-8'))  # Encode tag as bytes
+#         return accumulator
+#
+#     def merge_accumulators(self, accumulators):
+#         merged_hll = HyperLogLog()
+#         for hll in accumulators:
+#             merged_hll.merge(hll)
+#         return merged_hll
+#
+#     def extract_output(self, accumulator):
+#         return accumulator.count()
+#
+#
+# # Pipeline setup
+# with beam.Pipeline() as p:
+#     # Create a PCollection from the input data
+#     input_data = p | beam.Create(data)
+#
+#     # Apply the HyperLogLog transform
+#     total_distinct_count = (
+#             input_data
+#             | beam.Map(lambda element: element['tags'])
+#             | beam.CombineGlobally(HyperLogLogCombineFn())
+#     )
+#
+#     # Output the total count of unique tags
+#     total_distinct_count | beam.Map(print)
 
-# Sample input data
-data = [
-    {'tags': 'apple'},
-    {'tags': 'apple|banana'}
-]
+import csv
 
+tags = set()
+with open('tags_data.csv', 'r') as file:
+    reader = csv.reader(file)
+    next(reader, None)  # skip header
+    for row in reader:
+        for tag in row[0].split('|'):
+            tags.add(tag)
 
-# Custom combine function for HyperLogLog
-class HyperLogLogCombineFn(beam.CombineFn):
-    def create_accumulator(self):
-        return HyperLogLog()
-
-    def add_input(self, accumulator, element):
-        tags = element.split('|')  # Split the tags if there are multiple separated by '|'
-        for tag in tags:
-            accumulator.update(tag.encode('utf-8'))  # Encode tag as bytes
-        return accumulator
-
-    def merge_accumulators(self, accumulators):
-        merged_hll = HyperLogLog()
-        for hll in accumulators:
-            merged_hll.merge(hll)
-        return merged_hll
-
-    def extract_output(self, accumulator):
-        return accumulator.count()
-
-
-# Pipeline setup
-with beam.Pipeline() as p:
-    # Create a PCollection from the input data
-    input_data = p | beam.Create(data)
-
-    # Apply the HyperLogLog transform
-    total_distinct_count = (
-            input_data
-            | beam.Map(lambda element: element['tags'])
-            | beam.CombineGlobally(HyperLogLogCombineFn())
-    )
-
-    # Output the total count of unique tags
-    total_distinct_count | beam.Map(print)
+print(len(tags))
