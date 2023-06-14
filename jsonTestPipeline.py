490bb49b3c25c3dabe9dcd6a27b62281aaff0ba5
Thomas Niestroj <Thomas.Niestroj@informatik.hs-fulda.de>
2023-06-14 21:41:36 +0200

    plot data added to testpipeline

--- jsonTestPipeline.py
+++ jsonTestPipeline.py
@@ -1,14 +1,14 @@
 import os
-
 import apache_beam as beam
 import json
 from datetime import datetime
+import matplotlib.pyplot as plt
+import collections
 
 # Path to the JSON file
-json_file_path = "C:/Users/Thomas/Studium/SteamProc/TestJSON.json"
+json_file_path = "C:/Users/Thomas/Downloads/TestJSON.json"
 
-output_file_path = f"C:/Users/Thomas/Studium/SteamProc/OutputJSON_{datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
-
+output_file_path = f"C:/Users/Thomas/Downloads/OutputJSON_{datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
 
 class ProcessJSON(beam.DoFn):
     def process(self, element):
@@ -18,6 +18,7 @@ class ProcessJSON(beam.DoFn):
         # Emit the creation_date as the key and the entire JSON element as the value
         yield creation_date, element
 
+data = []
 
 # Create a pipeline
 with beam.Pipeline() as pipeline:
@@ -33,16 +34,15 @@ with beam.Pipeline() as pipeline:
             json_data
             | "Group by Creation Date" >> beam.ParDo(ProcessJSON())
             | "GroupByKey" >> beam.GroupByKey()
+            | "Store results" >> beam.Map(lambda x: data.append((x[0], len(x[1]))))
     )
 
-    output = (
-            grouped_data
-            | "Format for Output" >> beam.Map(lambda element: json.dumps({element[0]: element[1]}))
-            | "Write to File" >> beam.io.WriteToText(output_file_path)
-    )
+# The data list now contains the grouped data.
+# We'll plot a histogram of the number of elements per group (i.e., per creation_date).
+dates, counts = zip(*data)
 
-# Print the grouped data
-grouped_data | beam.Map(print)
+fig, ax = plt.subplots()
+ax.bar(dates, counts)
+plt.xticks(fontsize=4)  # Rotate x-axis labels for readability
+plt.show()
 
-# Run the pipeline
-pipeline.run()
